07b772362b682a4499864156c1ab0cc5819529e5
Victor Volkov <vityan2504@gmail.com>
2023-11-24 17:01:02 +0300

    Написал два класса с наследованием

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+class First {
+   hello() {
+      console.log("Привет я метод родителя!");
+   }
+}
+
+
+class Second extends First {
+   hello() {
+      super.hello();
+      console.log("А я наследуемый метод!");
+   }
+}
+
+const second = new Second();
+
+second.hello();
